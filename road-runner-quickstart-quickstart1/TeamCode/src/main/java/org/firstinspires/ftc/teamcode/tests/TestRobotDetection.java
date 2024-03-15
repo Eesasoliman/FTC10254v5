d3c3a8787e3bbcfb6276a6fe0319ba6c0b6d114d
Naimy441 <naimywatermelon@gmail.com>
2024-03-15 14:00:30 -0500

    Improved test after DriveOpMode changes

--- road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestRobotDetection.java
+++ road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestRobotDetection.java
@@ -12,19 +12,25 @@ public class TestRobotDetection extends DriveOpMode {
     @Override
     public void runOpMode() {
         telemetry.setMsTransmissionInterval(50);
-        SampleMecanumDrive drive = initDriveOpMode();
+
+        boolean isBlueSide = false;
 
         telemetry.addLine("DO NOT CLICK START.");
-        telemetry.addLine("Building camera processor.");
+        telemetry.addLine("Building camera processor...");
         telemetry.update();
         initAprilTagProcessor();
-        sleep(1000);
+
+        telemetry.addLine("DO NOT CLICK START.");
+        telemetry.addLine("Waiting for camera processor to start streaming...");
+        telemetry.update();
+        waitForCamera();
 
         telemetry.addLine("DO NOT CLICK START.");
         telemetry.addLine("Starting detection (5 seconds max).");
+        telemetry.addData("isBlueSide", isBlueSide);
         telemetry.update();
 
-        if (detectRobot(false)) {
+        if (detectRobot(isBlueSide)) {
             telemetry.addLine("Detected robot or did not detect one tag.");
         } else
         {
