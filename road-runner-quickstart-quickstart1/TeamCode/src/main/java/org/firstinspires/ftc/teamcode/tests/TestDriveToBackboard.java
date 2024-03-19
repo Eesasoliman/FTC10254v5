c09f2ac33fa5faa118200e1afc75c603cfa9e45d
Naimy441 <105175634+Naimy441@users.noreply.github.com>
2024-03-19 14:12:29 -0500

    Update TestDriveToBackboard.java

--- road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestDriveToBackboard.java
+++ road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestDriveToBackboard.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.tests;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.resources.DriveOpMode;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Autonomous
+@Disabled
 public class TestDriveToBackboard extends DriveOpMode {
     @Override
     public void runOpMode() {
